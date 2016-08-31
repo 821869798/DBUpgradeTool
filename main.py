95fe0f5d30f3963bca032dc6696e5506872ef1e3
bufan <821869798@qq.com>
2016-08-31 10:28:48 +0800

    ...

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from MysqlUpgrade import MysqlUpgradeTool
+import os
 
 if __name__ == '__main__':
     mysql_uprage = MysqlUpgradeTool()
     mysql_uprage.upgrade()
+    os.system("Pause")
